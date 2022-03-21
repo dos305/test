682abf055d8a1321b8b73fc80cef45ce6a193c77
od <oleh.demyd@lnu.edu.ua>
2022-03-21 15:42:06 +0200

    Add

---
+++ Plugin/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace Plugin
+{
+    public class Plugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+
+            IPluginExecutionContext context = (IPluginExecutionContext)
+            serviceProvider.GetService(typeof(IPluginExecutionContext));
+            Entity entity = (Entity)context.InputParameters["Target"];
+
+            if (entity.Contains("crf5a_phonenumber"))
+            {
+                string num = (string)entity["crf5a_phonenumber"];
+
+                if (num.Equals(null))
+                {
+                    entity["crf5a_isValidated"] = false;
+                }
+
+                else
+                {
+                    entity["crf5a_isValidated"] = true;
+                }
+            }
+
+        }
+    }
+}
